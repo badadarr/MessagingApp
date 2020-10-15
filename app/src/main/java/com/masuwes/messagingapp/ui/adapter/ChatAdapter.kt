74d31a8caf638794f35e37e53947aef2457a1b90
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-10-15 11:20:16 +0700

    error at MessageChatActivity.kt and ChatAdapter.kt

--- app/src/main/java/com/masuwes/messagingapp/ui/adapter/ChatAdapter.kt
+++ app/src/main/java/com/masuwes/messagingapp/ui/adapter/ChatAdapter.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -16,6 +17,10 @@ import com.masuwes.messagingapp.model.Chat
 import com.masuwes.messagingapp.model.ChatList
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.message_item_left.view.*
+import kotlinx.android.synthetic.main.message_item_left.view.show_text_message
+import kotlinx.android.synthetic.main.message_item_left.view.text_seen
+import kotlinx.android.synthetic.main.message_item_right.view.*
 
 class ChatAdapter(var context: Context, var chatList: List<Chat>, var imageUrl: String)
     : RecyclerView.Adapter<ChatAdapter.ViewHolder>() {
@@ -27,7 +32,7 @@ class ChatAdapter(var context: Context, var chatList: List<Chat>, var imageUrl: 
             val view = LayoutInflater.from(context).inflate(R.layout.message_item_right, parent, false)
             ViewHolder(view)
         } else {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.message_item_left, parent, false)
+            val view = LayoutInflater.from(context).inflate(R.layout.message_item_left, parent, false)
             ViewHolder(view)
         }
     }
@@ -35,10 +40,10 @@ class ChatAdapter(var context: Context, var chatList: List<Chat>, var imageUrl: 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val chat = chatList[position]
 
-        Picasso.get().load(imageUrl).into(holder.profileImage)
+        if(holder.profileImage != null) if(imageUrl.isNotBlank()) Picasso.get().load(imageUrl).into(holder.profileImage)
 
-        // image message - right side
-        if (chat.message.equals("sent you an image") && !chat.url.equals("")) {
+        /** image message - right side **/
+        if (chat.message.equals("sent you an image") && !chat.url.isNullOrEmpty()) {
             // image message - right side
             if (chat.sender.equals(firebaseUser.uid)) {
                 holder.showTextMessage?.visibility = View.GONE
@@ -46,10 +51,10 @@ class ChatAdapter(var context: Context, var chatList: List<Chat>, var imageUrl: 
                 Picasso.get().load(chat.url).into(holder.rightImageView)
             }
 
-            // image message - left side
+            /** image message - left side **/
             else if (!chat.sender.equals(firebaseUser.uid)) {
                 holder.showTextMessage?.visibility = View.GONE
-                holder.leftImageView?.visibility = View.VISIBLE
+                holder.leftImageView.visibility = View.VISIBLE
                 Picasso.get().load(chat.url).into(holder.leftImageView)
             }
         } else {
@@ -62,18 +67,18 @@ class ChatAdapter(var context: Context, var chatList: List<Chat>, var imageUrl: 
             if (chat.isseen!!) {
                 holder.textSeen?.text = context.resources.getString(R.string.seen)
 
-                if (chat.message.equals("sent you an image") && !chat.url.equals("")) {
-                    val lp: RelativeLayout.LayoutParams = holder.textSeen?.layoutParams as RelativeLayout.LayoutParams
+                if (chat.message == "sent you an image" && !chat.url.isNullOrEmpty()) {
+                    val lp = holder.textSeen?.layoutParams as ConstraintLayout.LayoutParams
                     lp.setMargins(0, 245, 10, 0)
-                    holder.textSeen?.layoutParams = lp
+                    holder.textSeen.layoutParams = lp
                 }
             } else {
                 holder.textSeen?.text = context.resources.getString(R.string.sent)
 
-                if (chat.message.equals("sent you an image") && !chat.url.equals("")) {
-                    val lp: RelativeLayout.LayoutParams = holder.textSeen?.layoutParams as RelativeLayout.LayoutParams
+                if (chat.message == "sent you an image" && !chat.url.isNullOrEmpty()) {
+                    val lp = holder.textSeen?.layoutParams as ConstraintLayout.LayoutParams
                     lp.setMargins(0, 245, 10, 0)
-                    holder.textSeen?.layoutParams = lp
+                    holder.textSeen.layoutParams = lp
                 }
             }
         } else {
@@ -84,22 +89,15 @@ class ChatAdapter(var context: Context, var chatList: List<Chat>, var imageUrl: 
     override fun getItemCount(): Int = chatList.size
 
     override fun getItemViewType(position: Int): Int {
-        return super.getItemViewType(position)
-
-
-        return if (chatList[position].sender.equals(firebaseUser.uid)) {
-            1
-        } else {
-            0
-        }
+        return if (chatList[position].sender.equals(firebaseUser.uid)) 1 else 0
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var profileImage: CircleImageView? = itemView.findViewById(R.id.friend_profile_left)
-        var showTextMessage: TextView? = itemView.findViewById(R.id.show_text_message)
-        var textSeen: TextView? = itemView.findViewById(R.id.text_seen)
-        var leftImageView: ImageView? = itemView.findViewById(R.id.image_view_left)
-        var rightImageView: ImageView? = itemView.findViewById(R.id.image_view_right)
+        var profileImage = itemView.friend_profile_left
+        var showTextMessage = itemView.show_text_message
+        var textSeen = itemView.text_seen
+        var leftImageView = itemView.image_view_left
+        var rightImageView = itemView.image_view_right
 
     }
 
